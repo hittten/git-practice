ba501919dfa06986aacce13aef30e19558009ec1
Roger Quilez Palleja <rogeerquilezpalleja@gmail.com>
2021-09-24 12:56:01 +0200

    Adding variable and function definitions

---
+++ src/Practice_RogerQuilez/RogerQuilez_JS1/js/main.js
@@ -0,0 +1,11 @@
+/* 1. COMPLETE VARIABLE AND FUNCTION DEFINITIONS */
+
+const customName = document.getElementById('customname');
+const randomize = document.querySelector('.randomize');
+const story = document.querySelector('.story');
+
+function randomValueFromArray(array){
+    const random = Math.floor(Math.random()*array.length);
+    return array[random];
+  }
+  

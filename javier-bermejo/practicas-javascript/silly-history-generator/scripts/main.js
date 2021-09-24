0c752b36fccdaef334994d2fe05a0370db793d09
Javier Bermejo Rodriguez <jotabecode@gmail.com>
2021-09-24 14:54:37 +0200

    event listener and partial function definition

--- javier-bermejo/practicas-javascript/silly-history-generator/scripts/main.js
+++ javier-bermejo/practicas-javascript/silly-history-generator/scripts/main.js
@@ -8,6 +8,30 @@ function randomValueFromArray(array) {
 }
 
 let storyText = "It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.";
-let insertX = "Willy the Goblin Big Daddy Father Christmas";
-let insertY = " the soup kitchen \n Disneyland \n the White House";
-let insertZ = " spontaneously combusted \n melted into a puddle on the sidewalk \n turned into a slug and crawled away ";
+let insertX = ["Willy the Goblin","Big Daddy","Father Christmas"];
+let insertY = ["the soup kitchen","Disneyland","the White House"];
+let insertZ = ["spontaneously combusted","melted into a puddle on the sidewalk","turned into a slug and crawled away "];
+
+let newStory = storyText;
+let xItem = randomValueFromArray(insertX);
+let yItem = randomValueFromArray(insertY);
+let zItem = randomValueFromArray(insertZ);
+
+randomize.addEventListener('click', result);
+
+function result() {
+
+  if(customName.value !== '') {
+    let name = customName.value;
+
+  }
+
+  if(document.getElementById("uk").checked) {
+    let weight = Math.round(300);
+    let temperature =  Math.round(94);
+
+  }
+
+  /*story.textContent = newStory;*/
+  story.style.visibility = 'visible';
+}

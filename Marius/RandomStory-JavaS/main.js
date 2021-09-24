6ab2f0aefd3a08bd8f2e2db1d586ed584225f3ef
Marius Marco <marcoioan20@gmail.com>
2021-09-24 19:07:11 +0200

    Practica

--- Marius/RandomStory-JavaS/main.js
+++ Marius/RandomStory-JavaS/main.js
@@ -9,10 +9,11 @@ function randomValueFromArray(array) {
 
 }
 
-let storyText="I`m trying so hard to learn html,css and javascrip and i'm not good at this things because ";
+let storyText="I`m trying so hard to learn how to make :insertx:, but i'm not good at this things because i'm :inserty: and planing to go :insertz: in search of :inserta:";
+let insertA =["weed", "knowledge", "bitches"];
 let insertX =["Chocolate", "Juice", "Drugs"];
-let insertY =["Kill", "Thinking" , "Flying"];
-let insertZ =["in the hood","the cataclism","is a dangerous stuff to smoke"];
+let insertY =["Planting", "Thinking" , "Wearing makeup"];
+let insertZ =["in the hood","on internet","in siria"];
 
 
 
@@ -21,11 +22,12 @@ randomize.addEventListener('click', result);
 function result() {
     let newStory = storyText;
 
+    let aItem = randomValueFromArray(insertA);
     let xItem = randomValueFromArray(insertX);
     let yItem = randomValueFromArray(insertY);
     let zItem = randomValueFromArray(insertZ);
 
-    newStory = newStory.replace(':insertx:',xItem);
+    newStory = newStory.replace(':inserta:',aItem);
     newStory = newStory.replace(':insertx:',xItem);
     newStory = newStory.replace(':inserty:',yItem);
     newStory = newStory.replace(':insertz:',zItem);

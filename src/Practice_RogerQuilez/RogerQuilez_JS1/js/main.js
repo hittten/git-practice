b1a6d614589b53f909f423a38aa90fda60e8497f
Roger Quilez Palleja <rogeerquilezpalleja@gmail.com>
2021-09-24 12:59:57 +0200

    Assing Raw text Strings

--- src/Practice_RogerQuilez/RogerQuilez_JS1/js/main.js
+++ src/Practice_RogerQuilez/RogerQuilez_JS1/js/main.js
@@ -4,8 +4,17 @@ const customName = document.getElementById('customname');
 const randomize = document.querySelector('.randomize');
 const story = document.querySelector('.story');
 
-function randomValueFromArray(array){
-    const random = Math.floor(Math.random()*array.length);
+function randomValueFromArray(array) {
+    const random = Math.floor(Math.random() * array.length);
     return array[random];
-  }
+}
+
+/* 2. RAW TEXT STRINGS */
+
+let storyText = "It was 94 fahrenheit outside, so : insertx: went for a walk.When they got to: inserty:, they stared in horror for a few moments, then : insertz:.Bob saw the whole thing, but was not surprised — : insertx: weighs 300 pounds, and it was a hot day.";
   
+let insertX = ['Willy the Goblin', 'Big Daddy', 'Father Christmas'];
+  
+let insertY = ['the soup kitchen', 'Disneyland', 'The White House'];
+  
+let insertZ = ['spontaneously combusted', 'melted into a puddle on the sidewalk', 'turned into a slug and crawled away'];

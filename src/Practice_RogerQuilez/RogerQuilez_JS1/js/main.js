43e3e672ff841723be501117e82ac7bf778fcc98
Roger Quilez Palleja <rogeerquilezpalleja@gmail.com>
2021-09-24 13:02:09 +0200

    Adding event listener and partial function definition

--- src/Practice_RogerQuilez/RogerQuilez_JS1/js/main.js
+++ src/Practice_RogerQuilez/RogerQuilez_JS1/js/main.js
@@ -12,9 +12,27 @@ function randomValueFromArray(array) {
 /* 2. RAW TEXT STRINGS */
 
 let storyText = "It was 94 fahrenheit outside, so : insertx: went for a walk.When they got to: inserty:, they stared in horror for a few moments, then : insertz:.Bob saw the whole thing, but was not surprised — : insertx: weighs 300 pounds, and it was a hot day.";
-  
-let insertX = ['Willy the Goblin', 'Big Daddy', 'Father Christmas'];
-  
-let insertY = ['the soup kitchen', 'Disneyland', 'The White House'];
-  
+let insertX = ['Willy the Goblin', 'Big Daddy', 'Father Christmas'];  
+let insertY = ['the soup kitchen', 'Disneyland', 'The White House'];  
 let insertZ = ['spontaneously combusted', 'melted into a puddle on the sidewalk', 'turned into a slug and crawled away'];
+
+/* 3. EVENT LISTENER AND PARTIAL FUNCTION DEFINITION */
+
+randomize.addEventListener('click', result);
+
+function result() {
+
+  if(customName.value !== '') {
+    let name = customName.value;
+
+  }
+
+  if(document.getElementById("uk").checked) {
+    let weight = Math.round(300);
+    let temperature =  Math.round(94);
+
+  }
+
+  story.textContent = ;
+  story.style.visibility = 'visible';
+}

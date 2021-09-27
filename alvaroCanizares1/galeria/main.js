402ba71b664146fffdb2492696032411e9b0e076
Alvaro Canizares <alvaro.canizares@babel.es>
2021-09-27 17:16:38 +0200

    finalizada

--- alvaroCanizares1/galeria/main.js
+++ alvaroCanizares1/galeria/main.js
@@ -6,8 +6,26 @@ const overlay = document.querySelector('.overlay');
 
 /* Looping through images */
 
-const newImage = document.createElement('img');
-newImage.setAttribute('src', xxx);
-thumbBar.appendChild(newImage);
+
+    for (let i = 1;i <=5; i++){
+        const newImage = document.createElement('img');
+        newImage.setAttribute('src', `images/pic${i}.jpg`);
+        thumbBar.appendChild(newImage);
+    }
+thumbBar.addEventListener('click', (e)=>{
+    displayedImage.setAttribute('src', e.target.src)
+})
+
 
 /* Wiring up the Darken/Lighten button */
+btn.addEventListener('click',(e)=>{
+    if(btn.className === 'dark'){
+        btn.setAttribute('class','light');
+        btn.textContent = 'Lighten';
+        overlay.style.backgroundColor = "rgba(0,0,0,0.5)";
+    }else{
+        btn.setAttribute('class','dark');
+        btn.textContent = 'Darken';
+        overlay.style.backgroundColor = "rgba(0,0,0,0)";
+    }
+})

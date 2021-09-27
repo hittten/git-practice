b936267a661adcb607004e5682229c014a925071
Roger Quilez Palleja <rogeerquilezpalleja@gmail.com>
2021-09-27 16:29:21 +0200

    Iniciando el proyecto y añadiendo los eventos a las imágenes

---
+++ src/Practice_RogerQuilez/gallery-start/main.js
@@ -0,0 +1,20 @@
+const displayedImage = document.querySelector('.displayed-img');
+const thumbBar = document.querySelector('.thumb-bar');
+
+const btn = document.querySelector('button');
+const overlay = document.querySelector('.overlay');
+
+/* Looping through images */
+
+for (let i = 0; i < 5; i ++) {
+    const newImage = document.createElement('img');
+    newImage.setAttribute('src', 'images/pic' + (i+1) + '.jpg');
+    thumbBar.appendChild(newImage);
+    newImage.addEventListener('click', () => {
+        displayedImage.setAttribute('src', newImage.getAttribute('src'));
+    })
+}
+
+
+
+/* Wiring up the Darken/Lighten button */

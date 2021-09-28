b189cae32e927b7f10b46730a12062291452366b
Alvaro Canizares <alvaro.canizares@babel.es>
2021-09-28 16:20:29 +0200

    finalizado

--- alvaroCanizares1/bouncing-balls/main.js
+++ alvaroCanizares1/bouncing-balls/main.js
@@ -19,155 +19,156 @@ function random(min,max) {
 }
 
 // define shape constructor
-
-function Shape(x, y, velX, velY, exists) {
-    this.x = x;
-    this.y = y;
-    this.velX = velX;
-    this.velY = velY;
-    this.exists = exists;
+class Shape {
+    constructor(x, y, velX, velY, exists) {
+        this.x = x;
+        this.y = y;
+        this.velX = velX;
+        this.velY = velY;
+        this.exists = exists;
+    }
 }
 
 // define Ball constructor, inheriting from Shape
 
-function Ball(x, y, velX, velY, exists, color, size) {
-    Shape.call(this, x, y, velX, velY, exists);
-
+class Ball extends Shape{
+    constructor(x, y, velX, velY, exists, color, size) {
+        super(x, y, velX, velY, exists);
     this.color = color;
     this.size = size;
-}
-
-Ball.prototype = Object.create(Shape.prototype);
-Ball.prototype.constructor = Ball;
-
-// define ball draw method
-
-Ball.prototype.draw = function() {
-    ctx.beginPath();
-    ctx.fillStyle = this.color;
-    ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
-    ctx.fill();
-};
-
-// define ball update method
-
-Ball.prototype.update = function() {
-    if((this.x + this.size) >= width) {
-        this.velX = -(this.velX);
     }
 
-    if((this.x - this.size) <= 0) {
-        this.velX = -(this.velX);
-    }
+    // define ball draw method
 
-    if((this.y + this.size) >= height) {
-        this.velY = -(this.velY);
-    }
+    draw() {
+        ctx.beginPath();
+        ctx.fillStyle = this.color;
+        ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+        ctx.fill();
+    };
 
-    if((this.y - this.size) <= 0) {
-        this.velY = -(this.velY);
-    }
+    // define ball update method
 
-    this.x += this.velX;
-    this.y += this.velY;
-};
+    update() {
+        if((this.x + this.size) >= width) {
+            this.velX = -(this.velX);
+        }
 
-// define ball collision detection
+        if((this.x - this.size) <= 0) {
+            this.velX = -(this.velX);
+        }
 
-Ball.prototype.collisionDetect = function() {
-    for(var j = 0; j < balls.length; j++) {
-        if(!(this === balls[j])) {
-            var dx = this.x - balls[j].x;
-            var dy = this.y - balls[j].y;
-            var distance = Math.sqrt(dx * dx + dy * dy);
+        if((this.y + this.size) >= height) {
+            this.velY = -(this.velY);
+        }
 
-            if (distance < this.size + balls[j].size && balls[j].exists) {
-                balls[j].color = this.color = 'rgb(' + random(0,255) + ',' + random(0,255) + ',' + random(0,255) +')';
+        if((this.y - this.size) <= 0) {
+            this.velY = -(this.velY);
+        }
+
+        this.x += this.velX;
+        this.y += this.velY;
+    };
+
+    // define ball collision detection
+
+    collisionDetect() {
+        for (let j = 0; j < balls.length; j++) {
+            if (!(this === balls[j]) && balls[j].exists) {
+                const dx = this.x - balls[j].x;
+                const dy = this.y - balls[j].y;
+                const distance = Math.sqrt(dx * dx + dy * dy);
+
+                if (distance < this.size + balls[j].size) {
+                    balls[j].color = this.color = 'rgb(' + random(0, 255) + ',' + random(0, 255) + ',' + random(0, 255) +')';
+                }
             }
         }
     }
-};
+
+}
+// Ball.prototype = Object.create(Shape.prototype);
+// Ball.prototype = Ball;
+//
+
 
 // define EvilCircle constructor, inheriting from Shape
 
-function EvilCircle(x, y, exists) {
-    Shape.call(this, x, y, 20, 20, exists);
-
-    this.color = 'white';
-    this.size = 10;
-}
-
-EvilCircle.prototype = Object.create(Shape.prototype);
-EvilCircle.prototype.constructor = EvilCircle;
-
-
-// define EvilCircle draw method
-
-EvilCircle.prototype.draw = function() {
-    ctx.beginPath();
-    ctx.strokeStyle = this.color;
-    ctx.lineWidth = 3;
-    ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
-    ctx.stroke();
-};
-
-
-// define EvilCircle checkBounds method
-
-EvilCircle.prototype.checkBounds = function() {
-    if((this.x + this.size) >= width) {
-        this.x -= this.size;
+class EvilCircle extends Shape{
+    constructor(x, y,exists){
+        super(x,y,20,20,exists);
+        this.color = 'white';
+        this.size = 10;
     }
 
-    if((this.x - this.size) <= 0) {
-        this.x += this.size;
-    }
+    // define EvilCircle draw method
 
-    if((this.y + this.size) >= height) {
-        this.y -= this.size;
-    }
+    draw() {
+        ctx.beginPath();
+        ctx.strokeStyle = this.color;
+        ctx.lineWidth = 3;
+        ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+        ctx.stroke();
+    };
+    // define EvilCircle checkBounds method
 
-    if((this.y - this.size) <= 0) {
-        this.y += this.size;
-    }
-};
+   checkBounds() {
+        if((this.x + this.size) >= width) {
+            this.x -= this.size;
+        }
+
+        if((this.x - this.size) <= 0) {
+            this.x += this.size;
+        }
+
+        if((this.y + this.size) >= height) {
+            this.y -= this.size;
+        }
+
+        if((this.y - this.size) <= 0) {
+            this.y += this.size;
+        }
+    };
 
 // define EvilCircle setControls method
 
-EvilCircle.prototype.setControls = function() {
-    var _this = this;
-    window.onkeydown = function(e) {
-        if(e.key === 'a') {
-            _this.x -= _this.velX;
-        } else if(e.key === 'd') {
-            _this.x += _this.velX;
-        } else if(e.key === 'w') {
-            _this.y -= _this.velY;
-        } else if(e.key === 's') {
-            _this.y += _this.velY;
-        }
+    setControls() {
+        let _this = this;
+        window.onkeydown = function(e) {
+            if(e.key === 'a') {
+                _this.x -= _this.velX;
+            } else if(e.key === 'd') {
+                _this.x += _this.velX;
+            } else if(e.key === 'w') {
+                _this.y -= _this.velY;
+            } else if(e.key === 's') {
+                _this.y += _this.velY;
+            }
+        };
     };
-};
 
 // define EvilCircle collision detection
 
-EvilCircle.prototype.collisionDetect = function() {
-    for(let j = 0; j < balls.length; j++) {
-        if( balls[j].exists ) {
-            const dx = this.x - balls[j].x;
-            const dy = this.y - balls[j].y;
-            const distance = Math.sqrt(dx * dx + dy * dy);
+    collisionDetect () {
+        for(let j = 0; j < balls.length; j++) {
+            if( balls[j].exists ) {
+                const dx = this.x - balls[j].x;
+                const dy = this.y - balls[j].y;
+                const distance = Math.sqrt(dx * dx + dy * dy);
 
-            if (distance < this.size + balls[j].size) {
-                balls[j].exists = false;
-                count--;
-                para.textContent = 'Ball count: ' + count;
+                if (distance < this.size + balls[j].size) {
+                    balls[j].exists = false;
+                    count--;
+                    para.textContent = 'Ball count: ' + count;
+                }
             }
         }
-    }
-};
+    };
 
+}
 
+// EvilCircle.prototype = Object.create(Shape.prototype);
+// EvilCircle.prototype = EvilCircle;
 
 // define array to store balls and populate it
 
@@ -214,7 +215,5 @@ function loop() {
 
     requestAnimationFrame(loop);
 }
-
-
 
 loop();

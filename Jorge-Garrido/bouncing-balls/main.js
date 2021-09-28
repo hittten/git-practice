ebb07dfff01e3cfd0fc4e67100cfb96061bf5f28
Jorge Garrido <jorge.garrido@babel.es>
2021-09-28 16:11:55 +0200

    subo los cambios del main.js en formato ES6

--- Jorge-Garrido/bouncing-balls/main.js
+++ Jorge-Garrido/bouncing-balls/main.js
@@ -13,8 +13,9 @@ const height = canvas.height = window.innerHeight;
 
 // function to generate random number
 
-function random(min,max) {
-    const num = Math.floor(Math.random()*(max-min)) + min;
+// Cambiado a función flecha
+let random = (min, max) => {
+    const num = Math.floor(Math.random() * (max - min)) + min;
     return num;
 }
 
@@ -28,13 +29,28 @@ function Shape(x, y, velX, velY, exists) {
     this.exists = exists;
 }
 
+// CAMBIAR CONSTRUCTOR NORMAL POR EL DE 'ES6', no me deja pero en el de abajo sí
+
+/* class Shape {
+    constructor(x, y, velX, velY, exists) {
+        this.x = x;
+        this.y = y;
+        this.velX = velX;
+        this.velY = velY;
+        this.exists = exists;
+    }
+} */
+
 // define Ball constructor, inheriting from Shape
 
-function Ball(x, y, velX, velY, exists, color, size) {
-    Shape.call(this, x, y, velX, velY, exists);
+// Cambiado el constructor
+class Ball {
+    constructor(x, y, velX, velY, exists, color, size) {
+        Shape.call(this, x, y, velX, velY, exists);
 
-    this.color = color;
-    this.size = size;
+        this.color = color;
+        this.size = size;
+    }
 }
 
 Ball.prototype = Object.create(Shape.prototype);
@@ -52,19 +68,19 @@ Ball.prototype.draw = function() {
 // define ball update method
 
 Ball.prototype.update = function() {
-    if((this.x + this.size) >= width) {
+    if ((this.x + this.size) >= width) {
         this.velX = -(this.velX);
     }
 
-    if((this.x - this.size) <= 0) {
+    if ((this.x - this.size) <= 0) {
         this.velX = -(this.velX);
     }
 
-    if((this.y + this.size) >= height) {
+    if ((this.y + this.size) >= height) {
         this.velY = -(this.velY);
     }
 
-    if((this.y - this.size) <= 0) {
+    if ((this.y - this.size) <= 0) {
         this.velY = -(this.velY);
     }
 
@@ -75,14 +91,14 @@ Ball.prototype.update = function() {
 // define ball collision detection
 
 Ball.prototype.collisionDetect = function() {
-    for(var j = 0; j < balls.length; j++) {
-        if(!(this === balls[j])) {
+    for (var j = 0; j < balls.length; j++) {
+        if (!(this === balls[j])) {
             var dx = this.x - balls[j].x;
             var dy = this.y - balls[j].y;
             var distance = Math.sqrt(dx * dx + dy * dy);
 
             if (distance < this.size + balls[j].size && balls[j].exists) {
-                balls[j].color = this.color = 'rgb(' + random(0,255) + ',' + random(0,255) + ',' + random(0,255) +')';
+                balls[j].color = this.color = 'rgb(' + random(0, 255) + ',' + random(0, 255) + ',' + random(0, 255) + ')';
             }
         }
     }
@@ -115,19 +131,19 @@ EvilCircle.prototype.draw = function() {
 // define EvilCircle checkBounds method
 
 EvilCircle.prototype.checkBounds = function() {
-    if((this.x + this.size) >= width) {
+    if ((this.x + this.size) >= width) {
         this.x -= this.size;
     }
 
-    if((this.x - this.size) <= 0) {
+    if ((this.x - this.size) <= 0) {
         this.x += this.size;
     }
 
-    if((this.y + this.size) >= height) {
+    if ((this.y + this.size) >= height) {
         this.y -= this.size;
     }
 
-    if((this.y - this.size) <= 0) {
+    if ((this.y - this.size) <= 0) {
         this.y += this.size;
     }
 };
@@ -137,13 +153,13 @@ EvilCircle.prototype.checkBounds = function() {
 EvilCircle.prototype.setControls = function() {
     var _this = this;
     window.onkeydown = function(e) {
-        if(e.key === 'a') {
+        if (e.key === 'a') {
             _this.x -= _this.velX;
-        } else if(e.key === 'd') {
+        } else if (e.key === 'd') {
             _this.x += _this.velX;
-        } else if(e.key === 'w') {
+        } else if (e.key === 'w') {
             _this.y -= _this.velY;
-        } else if(e.key === 's') {
+        } else if (e.key === 's') {
             _this.y += _this.velY;
         }
     };
@@ -152,8 +168,8 @@ EvilCircle.prototype.setControls = function() {
 // define EvilCircle collision detection
 
 EvilCircle.prototype.collisionDetect = function() {
-    for(let j = 0; j < balls.length; j++) {
-        if( balls[j].exists ) {
+    for (let j = 0; j < balls.length; j++) {
+        if (balls[j].exists) {
             const dx = this.x - balls[j].x;
             const dy = this.y - balls[j].y;
             const distance = Math.sqrt(dx * dx + dy * dy);
@@ -173,17 +189,17 @@ EvilCircle.prototype.collisionDetect = function() {
 
 const balls = [];
 
-while(balls.length < 25) {
-    const size = random(10,20);
+while (balls.length < 25) {
+    const size = random(10, 20);
     let ball = new Ball(
         // ball position always drawn at least one ball width
         // away from the adge of the canvas, to avoid drawing errors
-        random(0 + size,width - size),
-        random(0 + size,height - size),
-        random(-7,7),
-        random(-7,7),
+        random(0 + size, width - size),
+        random(0 + size, height - size),
+        random(-7, 7),
+        random(-7, 7),
         true,
-        'rgb(' + random(0,255) + ',' + random(0,255) + ',' + random(0,255) +')',
+        'rgb(' + random(0, 255) + ',' + random(0, 255) + ',' + random(0, 255) + ')',
         size
     );
     balls.push(ball);
@@ -193,15 +209,16 @@ while(balls.length < 25) {
 
 // define loop that keeps drawing the scene constantly
 
-let evil = new EvilCircle(random(0,width), random(0,height), true);
+let evil = new EvilCircle(random(0, width), random(0, height), true);
 evil.setControls();
 
-function loop() {
+// Cambiado a función flecha
+let loop = () => {
     ctx.fillStyle = 'rgba(0,0,0,0.25)';
-    ctx.fillRect(0,0,width,height);
+    ctx.fillRect(0, 0, width, height);
 
-    for(let i = 0; i < balls.length; i++) {
-        if(balls[i].exists) {
+    for (let i = 0; i < balls.length; i++) {
+        if (balls[i].exists) {
             balls[i].draw();
             balls[i].update();
             balls[i].collisionDetect();

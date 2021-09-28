d00efcf6a68e57ff23eb3de04c73c4de415c930d
MoaadSensei98 <moaad_5@hotmail.es>
2021-09-28 09:11:20 +0200

    Exercice imageGallery Javascript

---
+++ moaad-benjaafar/ImageGalery/script/main.js
@@ -0,0 +1,29 @@
+const displayedImage = document.querySelector('.displayed-img');
+const thumbBar = document.querySelector('.thumb-bar');
+
+const btn = document.querySelector('button');
+const overlay = document.querySelector('.overlay');
+
+/* Looping through images */
+for (let i = 1; i <= 5; i++) {
+    const newImage = document.createElement('img');
+    newImage.setAttribute('src', `images/pic${i}.jpg`);
+    thumbBar.appendChild(newImage);
+    newImage.addEventListener("click", () => {
+        displayedImage.setAttribute('src', newImage.getAttribute('src'));
+    });
+}
+
+
+/* Wiring up the Darken/Lighten button */
+   btn.addEventListener("click", ()=>{
+       if(btn.classList.contains('dark')){
+           overlay.style.backgroundColor =  'rgba(0,0,0,0.5)';
+           btn.setAttribute('class', 'light');
+           btn.textContent='Light';
+       }else{
+           overlay.style.backgroundColor =  'rgba(0,0,0,0.0)';
+           btn.setAttribute('class', 'dark');
+           btn.textContent='Dark';
+       }
+    })

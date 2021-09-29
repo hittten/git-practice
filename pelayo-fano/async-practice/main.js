d9498fa1ba42aaec49e42fc84f7d5c15c8db88a4
Pelayo Fano <pelayofano@gmail.com>
2021-09-29 17:59:12 +0200

    practica async acabada

--- pelayo-fano/async-practice/main.js
+++ pelayo-fano/async-practice/main.js
@@ -2,26 +2,30 @@ const defList = document.querySelector("#defList")
 
 
 fetch('data/persons.json')
-	.then(response => response.json())
-	.then((data) =>{
-        let personas = []
-        for (const names of data) {
-            const dt = document.createElement("dt")
-            defList.appendChild(dt)
-            dt.textContent = names.name
-            dt.id = names.name
-            personas.push(names.name)
+  .then(response => response.json())
+  .then((data) => {
+    let personas = []
+    for (const names of data) {
+      const dt = document.createElement("dt")
+      defList.appendChild(dt)
+      dt.textContent = names.name
+      dt.id = `${names.name}`
+      personas.push(names.name)
+    }
+
+    fetch('data/kittens.json')
+      .then(response => response.json())
+      .then((gatos => {
+        for (const persona of personas) {
+          console.log(gatos)
+          for (const gato of gatos[persona]) {
+            let asignar = document.querySelector(`#${persona}`)
+            const dd = document.createElement("dd")
+            dd.textContent = gato.name
+            asignar.appendChild(dd)
+          }
+
         }
-        console.log(personas[0])
-        fetch('data/kittens.json')
-            .then(response => response.json())
-            .then((gatos =>{              
-                
-              for (const persona of personas) {
-                console.log(gatos[persona])
-                    
-            }
-                
-            }))
-    } )
-  
+
+      }))
+  })

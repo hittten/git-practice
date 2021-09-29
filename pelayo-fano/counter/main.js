004d28341a0cede87a25e91924e5235c304e76e3
Pelayo Fano <pelayofano@gmail.com>
2021-09-29 16:13:47 +0200

    empezando timeout

--- pelayo-fano/counter/main.js
+++ pelayo-fano/counter/main.js
@@ -1,12 +1,33 @@
-//Actualizar boton:
 //seleccionar elementos del DOM y crear los que necesito
 const butContar = document.querySelector("#butContar")
 const input = document.querySelector("input")
 
-input.addEventListener("keyup",()=>{
+
+input.addEventListener("keyup",(e)=>{
     let segundos = input.value
     butContar.textContent = `Contar ${segundos} segundos`
-    if(segundos == ''){
+    //paso 2
+    if(segundos === ''){
         butContar.textContent = `Contar`
     }
+    //paso 3
+    if(e.key === "Enter"){     
+        input.select()
+        input.focus()
+        aniadirLista()
+     }
 })
+
+function aniadirLista(){
+    const ul = document.querySelector("#counters")
+    const li = document.createElement("li")
+    if(input.value !== ''){
+        let numero = input.value
+        li.textContent = numero
+        ul.appendChild(li)
+        li.addEventListener("click",()=>{
+            li.remove()
+        })
+    }     
+   
+}

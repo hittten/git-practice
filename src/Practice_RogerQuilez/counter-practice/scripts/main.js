54faf2a80df6e24beda25a0a10ac481f997120cd
Roger Quilez Palleja <rogeerquilezpalleja@gmail.com>
2021-09-29 15:23:39 +0200

    Adding TASK 4

--- src/Practice_RogerQuilez/counter-practice/scripts/main.js
+++ src/Practice_RogerQuilez/counter-practice/scripts/main.js
@@ -2,6 +2,7 @@
 /* TASK 2 - Si el input está vacío el botón debe decir solo "Contar" */
 const input = document.querySelector('input');
 const button = document.querySelector('button');
+const list = document.querySelector('#counters');
 
 input.addEventListener('keyup', (e) => {
     if (input.value) {
@@ -16,4 +17,26 @@ input.addEventListener('keydown', (e) => {
     if (e.key === 'Enter') {
         button.click();
     }
+})
+
+/* TASK 4 - Al momento de contar debe quedar enfocado el input con todo el valor seleccionado para que no se tenga que borrar */
+button.addEventListener('click', () => {
+
+    input.select();
+
+    let listCount = '';
+    let cont = 1;
+
+    const printList = setInterval(() => {
+
+        listCount = `<li>${cont}/${input.value}</li>`;
+        list.innerHTML = listCount;
+        if (cont == input.value) {
+            clearInterval(printList);
+        }
+        cont++; 
+        
+    }, 1000)
+
+    
 })

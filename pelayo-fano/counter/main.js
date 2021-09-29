da87ef019c56d23a4db59cded07c28bcc4b4b621
Pelayo Fano <pelayofano@gmail.com>
2021-09-29 15:44:21 +0200

    primer paso hecho

---
+++ pelayo-fano/counter/main.js
@@ -0,0 +1,9 @@
+//Actualizar boton:
+//seleccionar elementos del DOM y crear los que necesito
+const butContar = document.querySelector("#butContar")
+const input = document.querySelector("input")
+
+input.addEventListener("keyup",()=>{
+    let segundos = input.value
+    butContar.textContent = `Contar ${segundos} segundos`
+})
